21a84e5d649e9fbd00ffec42c159a78148ddf24c
exkc <exkc@exkc.moe>
2025-07-17 18:07:48 +0800

    a

--- js/light.js
+++ js/light.js
@@ -4,13 +4,16 @@ body.classList.add('light-mode');
 
 toggleButton.addEventListener('click', () => {
     discord = document.querySelector(".discordframe");
+    disexport = document.querySelector("#disexport");
     if (body.classList.contains('light-mode')) {
         body.classList.replace('light-mode', 'dark-mode');
 	     toggleButton.src = "/img/dark.svg";
+	    if (disexport) {disexport.setAttribute("src", "https://daktoinc.co.uk/disexport/dark/");}
 	    if (discord) {discord.setAttribute("src", "https://discord.com/widget?id=772434304009109525&theme=dark");}
     } else {
         body.classList.replace('dark-mode', 'light-mode');
 	     toggleButton.src = "/img/light.svg";
+	    if (disexport) {disexport.setAttribute("src", "https://daktoinc.co.uk/disexport/light/");}
 	    if (discord) {discord.setAttribute("src", "https://discord.com/widget?id=772434304009109525&theme=light");}
 
     }
