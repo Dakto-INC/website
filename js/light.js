46b096c7060b29b95254005e809ad5fc62fb560b
exkc <exkc@exkc.moe>
2025-07-17 09:59:29 +0100

    a

--- js/light.js
+++ js/light.js
@@ -6,9 +6,9 @@ body.classList.add('light-mode');
 toggleButton.addEventListener('click', () => {
     if (body.classList.contains('light-mode')) {
         body.classList.replace('light-mode', 'dark-mode');
-	     toggleButton.src = "/img/light.svg";
+	     toggleButton.src = "/img/dark.svg";
     } else {
         body.classList.replace('dark-mode', 'light-mode');
-	     toggleButton.src = "/img/dark.svg";
+	     toggleButton.src = "/img/light.svg";
     }
 });
